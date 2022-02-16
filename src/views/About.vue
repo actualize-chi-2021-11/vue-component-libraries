a9dbe57122e5aec2c54847a85ed278be7e51ad7d
Peter Jang <peter.x.jang@gmail.com>
2022-02-15 20:56:22 -0600

    Add multiselect componnent

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,22 @@
+<script>
+import VueMultiselect from "vue-multiselect";
+
+export default {
+  components: { VueMultiselect },
+  data() {
+    return {
+      selected: null,
+      options: ["list", "of", "options"],
+    };
+  },
+};
+</script>
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>Example multi-select</h2>
+    <VueMultiselect v-model="selected" :multiple="true" :options="options"></VueMultiselect>
     <h2>Example chart</h2>
     <line-chart :data="{ '2017-05-13': 2, '2017-05-14': 5 }"></line-chart>
     <pie-chart
@@ -11,3 +27,5 @@
     ></pie-chart>
   </div>
 </template>
+
+<style src="vue-multiselect/dist/vue-multiselect.css"></style>
