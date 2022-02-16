025d590b5d3d1e2b742a2eb5a4b2bfe0cf03cbff
Peter Jang <peter.x.jang@gmail.com>
2022-02-15 20:49:34 -0600

    Add chart component

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>Example chart</h2>
+    <line-chart :data="{ '2017-05-13': 2, '2017-05-14': 5 }"></line-chart>
+    <pie-chart
+      :data="[
+        ['Blueberry', 44],
+        ['Strawberry', 23],
+      ]"
+    ></pie-chart>
   </div>
 </template>
