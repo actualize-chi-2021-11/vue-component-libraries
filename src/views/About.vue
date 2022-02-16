83ff4b8d854adc45d9f0d66ccda0f1df0b47bdc4
Peter Jang <peter.x.jang@gmail.com>
2022-02-15 21:12:49 -0600

    Add carousel component

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,10 @@
 <script>
 import VueMultiselect from "vue-multiselect";
+import "vue3-carousel/dist/carousel.css";
+import { Carousel, Slide, Pagination, Navigation } from "vue3-carousel";
 
 export default {
-  components: { VueMultiselect },
+  components: { VueMultiselect, Carousel, Slide, Pagination, Navigation },
   data() {
     return {
       selected: null,
@@ -15,6 +17,28 @@ export default {
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>Example carousel</h2>
+    <Carousel>
+      <Slide v-for="slide in 10" :key="slide">
+        <div class="carousel__item">{{ slide }}</div>
+      </Slide>
+
+      <template #addons>
+        <Navigation />
+        <Pagination />
+      </template>
+    </Carousel>
+
+    <Carousel :itemsToShow="3.95" :wrapAround="true">
+      <Slide v-for="slide in 10" :key="slide">
+        <div class="carousel__item">{{ slide }}</div>
+      </Slide>
+
+      <template #addons>
+        <Pagination />
+      </template>
+    </Carousel>
+
     <h2>Example multi-select</h2>
     <VueMultiselect v-model="selected" :multiple="true" :options="options"></VueMultiselect>
     <h2>Example chart</h2>
@@ -29,3 +53,45 @@ export default {
 </template>
 
 <style src="vue-multiselect/dist/vue-multiselect.css"></style>
+<style>
+.carousel__item {
+  min-height: 200px;
+  width: 100%;
+  background-color: var(--vc-clr-primary);
+  color: var(--vc-clr-white);
+  font-size: 20px;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.carousel__slide {
+  padding: 10px;
+}
+
+.carousel__prev,
+.carousel__next {
+  box-sizing: content-box;
+  border: 5px solid white;
+}
+
+.carousel__slide > .carousel__item {
+  transform: scale(1);
+  opacity: 0.5;
+  transition: 0.5s;
+}
+.carousel__slide--visible > .carousel__item {
+  opacity: 1;
+  transform: rotateY(0);
+}
+.carousel__slide--next > .carousel__item {
+  transform: scale(0.9) translate(-10px);
+}
+.carousel__slide--prev > .carousel__item {
+  transform: scale(0.9) translate(10px);
+}
+.carousel__slide--active > .carousel__item {
+  transform: scale(1.1);
+}
+</style>
