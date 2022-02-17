d9e92ae6c9a5cce724a2c7ffd5430d4604cabbde
Peter Jang <peter.x.jang@gmail.com>
2022-02-16 20:57:29 -0600

    Use an array of place data to build markers with popups in a loop

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -2,6 +2,11 @@
 /* global mapboxgl */
 
 export default {
+  data: function () {
+    return {
+      places: [],
+    };
+  },
   mounted: function () {
     mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_API_KEY;
     const map = new mapboxgl.Map({
@@ -13,12 +18,19 @@ export default {
 
     console.log(map);
 
-    const popup = new mapboxgl.Popup({ offset: 25 }).setText(
-      "Construction on the Washington Monument began in 1848."
-    );
-
-    const marker1 = new mapboxgl.Marker().setLngLat([-87.6298, 41.8781]).setPopup(popup).addTo(map);
-    console.log(marker1);
+    // make a web request to get places from a backend...
+    this.places = [
+      { lat: -25.363, lng: 131.044, description: "A place in Australia" },
+      { lat: -33.8675, lng: 151.207, description: "The main city!" },
+    ];
+    this.places.forEach((place) => {
+      const popup = new mapboxgl.Popup({ offset: 25 }).setText(place.description);
+      const marker1 = new mapboxgl.Marker()
+        .setLngLat([place.lng, place.lat])
+        .setPopup(popup)
+        .addTo(map);
+      console.log(marker1);
+    });
   },
 };
 </script>
