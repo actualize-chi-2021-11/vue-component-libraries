7983df955a120b28a0bdde45350f0641ebb0df14
Peter Jang <peter.x.jang@gmail.com>
2022-02-16 20:11:56 -0600

    Add map to page

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,3 +1,32 @@
+<script>
+/* global mapboxgl */
+
+export default {
+  mounted: function () {
+    mapboxgl.accessToken = "_____";
+    const map = new mapboxgl.Map({
+      container: "map", // container ID
+      style: "mapbox://styles/mapbox/streets-v11", // style URL
+      center: [-87.6298, 41.8781], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+
+    console.log(map);
+  },
+};
+</script>
+
 <template>
   <h1>Map stuff</h1>
+  <div id="map"></div>
 </template>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+#map {
+  height: 500px;
+}
+</style>
