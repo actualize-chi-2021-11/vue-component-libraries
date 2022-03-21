ac098fc51b07c1d98e511574cce3b948309a8562
Peter Jang <peter.x.jang@gmail.com>
2022-03-21 18:58:46 -0500

    Add vue-draggable elements

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,8 +6,19 @@ import { Carousel, Slide, Pagination, Navigation } from "vue3-carousel";
 import "v-calendar/dist/style.css";
 import { Calendar, DatePicker } from "v-calendar";
 
+import { VueDraggableNext } from "vue-draggable-next";
+
 export default {
-  components: { Calendar, DatePicker, VueMultiselect, Carousel, Slide, Pagination, Navigation },
+  components: {
+    Calendar,
+    DatePicker,
+    VueMultiselect,
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation,
+    draggable: VueDraggableNext,
+  },
   data() {
     return {
       selected: null,
@@ -17,7 +28,20 @@ export default {
         "https://ichef.bbci.co.uk/news/999/cpsprodpb/15951/production/_117310488_16.jpg",
         "https://media.istockphoto.com/photos/hot-air-balloons-flying-over-the-botan-canyon-in-turkey-picture-id1297349747?b=1&k=20&m=1297349747&s=170667a&w=0&h=oH31fJty_4xWl_JQ4OIQWZKP8C6ji9Mz7L4XmEnbqRU=",
       ],
+      enabled: true,
+      list: [
+        { name: "John", id: 1 },
+        { name: "Joao", id: 2 },
+        { name: "Jean", id: 3 },
+        { name: "Gerard", id: 4 },
+      ],
+      dragging: false,
     };
+  },
+  methods: {
+    log(event) {
+      console.log(event);
+    },
   },
 };
 </script>
@@ -25,6 +49,16 @@ export default {
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>Example draggable</h2>
+    <draggable class="dragArea list-group w-full" :list="list" @change="log">
+      <div
+        class="list-group-item bg-gray-300 m-1 p-3 rounded-md text-center"
+        v-for="element in list"
+        :key="element.name"
+      >
+        {{ element.name }}
+      </div>
+    </draggable>
 
     <h2>Example calendar</h2>
     <Calendar />
